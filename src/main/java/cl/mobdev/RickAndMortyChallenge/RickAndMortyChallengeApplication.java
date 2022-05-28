9d2b67ed0ef87809d840cac6509513157231d422
christian vera vera <christian.vera@23people.io>
2022-05-27 23:13:26 -0400

    feature(root): init project

---
+++ src/main/java/cl/mobdev/RickAndMortyChallenge/RickAndMortyChallengeApplication.java
@@ -0,0 +1,13 @@
+package cl.mobdev.RickAndMortyChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickAndMortyChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/mobdev/RickAndMortyChallenge/RickAndMortyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mobdev.RickAndMortyChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
